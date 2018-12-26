ccf6b99754059765ab8688a4ecefb03602d711c3
Thomas Holt <twh@pobox.com>
2018-12-26 14:26:48 -0500

    Initial commit

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,43 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+
+describe package('centos-release-scl') do
+	it { should be_installed }
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe package('httpd24') do
+	it { should be_installed }
+end
+
+describe package('rh-php72-php-fpm') do
+	it { should be_installed }
+end
+
+describe directory('/opt/dokuwiki') do
+	it { should exist }
+end
+
+describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/dokuwiki.conf') do
+	it { should exist }
+end
+
+describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/php-fpm.conf') do
+	it { should exist }
+end
+
+describe service('rh-php72-php-fpm') do
+	it { should be_running }
+end
+
+describe service('httpd24-httpd') do
+	it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe command('curl http://localhost/dokuwiki/') do
+	its('stdout') { should match /dokuwiki/ }
 end

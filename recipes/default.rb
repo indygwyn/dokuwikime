6a5196a986600d0be834cf5b4d80cbfb6ec9e33c
Thomas Holt <twh@pobox.com>
2018-12-26 14:59:52 -0500

    fixed cookstyle problems

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,34 +7,34 @@
 
 package 'centos-release-scl'
 
-package %w{ httpd24 httpd24-mod_ssl rh-php72 rh-php72-php-cli rh-php72-php-fpm rh-php72-php-gd rh-php72-php-mbstring rh-php72-php-xml rh-php72-php-json }
+package %w( httpd24 httpd24-mod_ssl rh-php72 rh-php72-php-cli rh-php72-php-fpm rh-php72-php-gd rh-php72-php-mbstring rh-php72-php-xml rh-php72-php-json )
 
 ark 'dokuwiki' do
-	url 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz'
-	path '/opt'
-	owner 'apache'
-	group 'apache'
-	action :put
+  url 'https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz'
+  path '/opt'
+  owner 'apache'
+  group 'apache'
+  action :put
 end
 
 template '/opt/rh/httpd24/root/etc/httpd/conf.d/dokuwiki.conf' do
-	source 'dokuwiki.conf.erb'
-	mode '0644'
-	owner 'root'
-	group 'root'
+  source 'dokuwiki.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
 
 template '/opt/rh/httpd24/root/etc/httpd/conf.d/php-fpm.conf' do
-	source 'php-fpm.conf.erb'
-	mode '0644'
-	owner 'root'
-	group 'root'
+  source 'php-fpm.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
 
 service 'rh-php72-php-fpm' do
-	action [ :enable, :start ]
+  action [ :enable, :start ]
 end
 
 service 'httpd24-httpd' do
-	action [ :enable, :start ]
+  action [ :enable, :start ]
 end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,37 +5,36 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-
 describe package('centos-release-scl') do
-	it { should be_installed }
+  it { should be_installed }
 end
 
 describe package('httpd24') do
-	it { should be_installed }
+  it { should be_installed }
 end
 
 describe package('rh-php72-php-fpm') do
-	it { should be_installed }
+  it { should be_installed }
 end
 
 describe directory('/opt/dokuwiki') do
-	it { should exist }
+  it { should exist }
 end
 
 describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/dokuwiki.conf') do
-	it { should exist }
+  it { should exist }
 end
 
 describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/php-fpm.conf') do
-	it { should exist }
+  it { should exist }
 end
 
 describe service('rh-php72-php-fpm') do
-	it { should be_running }
+  it { should be_running }
 end
 
 describe service('httpd24-httpd') do
-	it { should be_running }
+  it { should be_running }
 end
 
 describe port(80) do
@@ -43,5 +42,5 @@ describe port(80) do
 end
 
 describe command('curl http://localhost/dokuwiki/') do
-	its('stdout') { should match /dokuwiki/ }
+  its('stdout') { should match /dokuwiki/ }
 end

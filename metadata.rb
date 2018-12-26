dc03aeb104b3f8f657e7e334ff726ee564244f21
Thomas Holt <twh@pobox.com>
2018-12-26 14:55:03 -0500

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,25 +1,12 @@
 name 'dokuwikime'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures dokuwikime'
-long_description 'Installs/Configures dokuwikime'
-version '0.1.0'
+maintainer 'Thomas Holt'
+maintainer_email 'twh@pobox.com'
+license 'MIT'
+description 'Installs dokuwiki on Apache 2.4/PHP 7.2 on CentOS'
+log_description 'Installs dokuwiki on Apache 2.4/PHP 7.2 on CentOS.'
+version '0.2.0'
 chef_version '>= 13.0'
-
 supports 'centos'
-
 depends 'ark'
-
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/dokuwikime/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/dokuwikime'
+issues_url 'https://github.com/indygwyn/dokuwikime/issues'
+source_url 'https://github.com/indygwyn/dokuwikime'

0c81ba0e494f1496f55d152817ada0eca2196882
indygwyn <twh@pobox.com>
2019-01-01 19:17:33 -0500

    reorganized tests

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,40 +5,30 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-describe package('centos-release-scl') do
-  it { should be_installed }
-end
-
 describe yum.repo('centos-sclo-rh') do 
   it { should be_enabled }
 end
 
-describe package('httpd24') do
-  it { should be_installed }
-end
-
-describe package('rh-php72-php-fpm') do
-  it { should be_installed }
+%w(centos-release-scl httpd24 rh-php72-php-fpm).each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
 end
 
 describe directory('/opt/dokuwiki') do
   it { should exist }
 end
 
-describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/dokuwiki.conf') do
-  it { should exist }
+%w(/opt/rh/httpd24/root/etc/httpd/conf.d/dokuwiki.conf /opt/rh/httpd24/root/etc/httpd/conf.d/php-fpm.conf).each do |fp|
+  describe file(fp) do
+    it { should exist }
+  end
 end
 
-describe file('/opt/rh/httpd24/root/etc/httpd/conf.d/php-fpm.conf') do
-  it { should exist }
-end
-
-describe service('rh-php72-php-fpm') do
-  it { should be_running }
-end
-
-describe service('httpd24-httpd') do
-  it { should be_running }
+%w(httpd24-httpd rh-php72-php-fpm).each do |svc|
+  describe service(svc) do
+    it { should be_running }
+  end
 end
 
 describe file('/opt/dokuwiki/data/pages/start.txt') do
